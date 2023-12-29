c091a34bba9fc1f33463d8951f4d6cba67bb6379
Alihmaidi <alihmaidi095@gmail.com>
2023-12-29 17:34:25 +0300

    configration api gateway

--- src/ApiGateways/OcelotGateway/Program.cs
+++ src/ApiGateways/OcelotGateway/Program.cs
@@ -1,6 +1,27 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOcelot();
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Configuration.AddJsonFile($"ocelot.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json");
+
 var app = builder.Build();
+
+
 
 app.MapGet("/", () => "Hello World!");
 
+app.UseSwaggerForOcelotUI(option =>
+{
+
+    option.PathToSwaggerGenerator = "/swagger/docs";
+
+});
+
+
+app.UseOcelot().Wait();
 app.Run();
